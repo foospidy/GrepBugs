5585a82a962001636cebf1681c214d4d9471369a
foospidy <foospidy@www.foospidy.com>
2014-11-04 22:16:27 -0500

    added support for scanning bitbucket and sourceforge repos

--- grepbugs.py
+++ grepbugs.py
@@ -81,9 +81,13 @@ def local_scan(srcdir, repo='none', account='local_scan', project='none'):
 	print 'counting source files...'
 	call(["cloc", "--sql=" + clocsql, "--sql-project=" + srcdir, srcdir])
 	# run sql script
-	f = open(clocsql, 'r')
-	cur.executescript(f.read())
-	f.close
+	try:
+		f = open(clocsql, 'r')
+		cur.executescript(f.read())
+		f.close
+	except:
+		print 'Error opening cloc sql file, perhaps no files in project.'
+		return scan_id
 
 	# execute clock again
 	call(["cloc", "--quiet", "-out=" + cloctxt, srcdir])
@@ -148,7 +152,14 @@ def local_scan(srcdir, repo='none', account='local_scan', project='none'):
 							# update databse with new results_detail info
 							result_detail_id = str(uuid.uuid1())
 							params           = [result_detail_id, result_id, rr[0], str(rr[1]).strip()]
-							cur.execute("INSERT INTO results_detail (result_detail_id, result_id, file, code) VALUES (?, ?, ?, ?);", params)
+
+							try:
+								cur.execute("INSERT INTO results_detail (result_detail_id, result_id, file, code) VALUES (?, ?, ?, ?);", params)
+							except lite.Error, e:
+								print params
+								print e
+								# should log this or something
+
 							db.commit()
 
 	params = [project_id]
@@ -177,53 +188,160 @@ def repo_scan(repo, account):
 	rows = cur.fetchall()
 
 	for row in rows:
-		cmd     = row[0]
 		co_url  = row[1].replace('ACCOUNT', account)
 		api_url = row[2].replace('ACCOUNT', account)
 
 		# call api_url
-		f    = urllib2.urlopen(api_url)
-		j    = f.read()
-		t    = '/tmp/gb.repo.' + str(os.getpid()) + '.tmp'
+		data = json.load(urllib2.urlopen(api_url))
 
-		with open(t, 'wb') as jsonfile:
-			jsonfile.write(j)
+		if 'github' == repo:
+			for i in range(0, len(data)):
+				do_scan      = True
+				project_name = data[i]["name"]
+				last_scanned = last_scan(repo, account, project_name)
+				last_changed = datetime.datetime.strptime(data[i]['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
+				checkout_url = 'https://github.com/' + account + '/' + project_name + '.git'
+				cmd          = 'git'
 
-		# load json data
-		json_file = open(t, "r")
-		data      = json.load(json_file)
-		json_file.close()
+				print project_name + ' last changed on ' + str(last_changed) + ' and last scanned on ' + str(last_scanned)
 
-		for i in range(0, len(data)):
-			do_scan = True
-			params  = [repo, account, data[i]['name']]
-			cur.execute("SELECT last_scan FROM projects WHERE repo=? AND account=? and project=?;", params)
-			rows = cur.fetchall()
-
-			for row in rows:
-				if None != row[0]:
-					last_change = datetime.datetime.strptime(data[i]['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
-					last_scan   = datetime.datetime.strptime(str(row[0]), "%Y-%m-%d %H:%M:%S")
-					print data[i]["name"] + ' last changed on ' + str(last_change) + ' and last scanned on ' + str(last_scan)
-					if last_change < last_scan:
+				if None != last_scanned:
+					if last_changed < last_scanned:
 						do_scan = False
 
-			if True == do_scan:
-				account_folder = os.path.dirname(os.path.abspath(__file__)) + '/src/' + account
+				if True == do_scan:
+					checkout_code(cmd, checkout_url, account, project_name)
+					# scan local files
+					local_scan(os.path.dirname(os.path.abspath(__file__)) + '/src/' + account, repo, account, project_name)
 
-				if not os.path.exists(account_folder):
-					os.makedirs(account_folder)
+		elif 'bitbucket' == repo:
+			for j in range(0, len(data["values"])):
+				value =  data["values"][j]
 
-				# checkout code
-				call(['rm', '-rf', account_folder + '/' + data[i]['name']])
-				call(['git', 'clone', data[i]['clone_url'], account_folder + '/' + data[i]['name']])
+				if 'git' == value['scm']:
+					do_scan      = True
+					project_name = str(value['full_name']).split('/')[1]
+					last_scanned = last_scan(repo, account, project_name)
+					date_split   = str(value['updated_on']).split('.')[0]
+					last_changed = datetime.datetime.strptime(date_split, "%Y-%m-%dT%H:%M:%S")
+					checkout_url = 'https://bitbucket.org/' + value['full_name']
+					cmd          = 'git'
 
-				# scan local files
-				local_scan(account_folder + '/' + data[i]['name'], repo, account, data[i]['name'])
+					print project_name + ' last changed on ' + str(last_changed) + ' and last scanned on ' + str(last_scanned)
 
+					if None != last_scanned:
+						if last_changed < last_scanned:
+							do_scan = False
+
+					if True == do_scan:
+						checkout_code(cmd, checkout_url, account, project_name)
+						# scan local files
+						local_scan(os.path.dirname(os.path.abspath(__file__)) + '/src/' + account, repo, account, project_name)
+
+		elif 'sourceforge' == repo:
+			for i in data['projects']:
+				do_scan      = True
+				project_name = i["url"].replace('/p/', '').replace('/', '')
+				cmd          = None 
+				project_json = json.load(urllib2.urlopen('https://sourceforge.net/rest' + i['url']))
+				for j in project_json:
+					for t in project_json['tools']:
+						if 'code' == t['mount_point']:
+							if 'git' == t['name']:
+								cmd          = 'git'
+								checkout_url = 'git://git.code.sf.net/p/' + str(project_name).lower() + '/code'
+							elif 'svn' == t['name']:
+								cmd          = 'svn'
+								checkout_url = 'svn://svn.code.sf.net/p/' + str(project_name).lower() + '/code'
+
+				last_scanned = last_scan(repo, account, project_name)
+				date_split   = i['last_updated'].split('.')[0]
+				last_changed = datetime.datetime.strptime(date_split, "%Y-%m-%d %H:%M:%S")
+
+				print project_name + ' last changed on ' + str(last_changed) + ' and last scanned on ' + str(last_scanned)
+
+				if None != last_scanned:
+					if last_changed < last_scanned:
+						do_scan = False
+
+				if True == do_scan:
+					if None != cmd:
+						checkout_code(cmd, checkout_url, account, project_name)
+						# scan local files
+						local_scan(os.path.dirname(os.path.abspath(__file__)) + '/src/' + account, repo, account, project_name)
+					else:
+						print 'No sourceforge repo for ' + account + ' ' + project_name
 
 		db.close()
 		print 'SCAN COMPLETE!'
+
+def checkout_code(cmd, checkout_url, account, project):
+	account_folder = os.path.dirname(os.path.abspath(__file__)) + '/src/' + account
+
+	if not os.path.exists(account_folder):
+		os.makedirs(account_folder)
+
+	# checkout code
+	call(['rm', '-rf', account_folder + '/' + project])
+	if 'git' == cmd:
+		print 'git clone...'
+		call(['git', 'clone', checkout_url, account_folder + '/' + project])
+	elif 'svn' == cmd:
+		# need to do a lot of craziness for svn, no wonder people use git now.
+		print 'svn checkout...'
+		found_trunk = False
+
+		call(['svn', '-q', 'checkout', '--depth', 'immediates', checkout_url, account_folder + '/tmp/' + project])
+
+		# look for first level trunks
+		for path, dirs, files in os.walk(os.path.abspath(account_folder + '/tmp/' + project)):
+			for i in range(0, len(dirs)):
+				if 'trunk' == dirs[i]:
+					if os.path.isdir(path + '/' + dirs[i]):
+						found_trunk = True
+						print 'co ' + checkout_url + '/' + dirs[i]
+						call(['svn', '-q', 'checkout', checkout_url + '/' + dirs[i], account_folder + '/' + project])
+
+		if False == found_trunk:
+			# try looking for tunk in second level
+			path = os.path.abspath(account_folder + '/tmp/' + project)
+			for n in os.listdir(path):
+				if os.path.isdir(path + '/' + n):
+					if '.svn' != n:
+						print 'co ' + checkout_url + '/' + n + '/trunk'
+						return_code = call(['svn', '-q', 'checkout', checkout_url + '/' + n + '/trunk', account_folder + '/' + project])
+						if 0 == return_code:
+							found_trunk = True
+
+		if False == found_trunk:
+			# didn't find a trunk, so checkout of last resort
+			print 'WARNING: no trunk found so checking out everything. This could take a while and consume disk space if there are many branches.'
+			call(['svn', '-q', 'checkout', checkout_url, account_folder + '/' + project])
+
+		# remove temp checkout
+		call(['rm', '-rf', os.path.abspath(account_folder + '/tmp/')])
+
+
+def last_scan(repo, account, project):
+	try:
+		db  = lite.connect(dbfile)
+		cur = db.cursor()
+
+	except lite.Error, e:
+		print 'Error connecting to db file'
+		sys.exit(1)
+
+	params = [repo, account, project]
+	cur.execute("SELECT last_scan FROM projects WHERE repo=? AND account=? and project=?;", params)
+	rows      = cur.fetchall()
+	last_scan = None
+	
+	for row in rows:
+		if None != row[0]:
+			last_scan = datetime.datetime.strptime(str(row[0]), "%Y-%m-%d %H:%M:%S")
+
+	db.close()
+	return last_scan
 
 def html_report(scan_id):
 	"""
